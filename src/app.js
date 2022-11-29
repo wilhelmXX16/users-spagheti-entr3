ead62bdc06d8544599b636f8d461f8be581c8b67
wilhelmXX16 <wjc1619t@gmail.com>
2022-11-29 17:43:35 -0500

    finish configure app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,36 @@
 const express = require('express')
 
-const port = 9000
+
+const db = require('./utils/database')
+const usersRouter = require('./users/users.routers')
 
 const app = express()
+
+const port = 9000
+
+db.authenticate()
+    .then(() => {
+        console.log('Database Authenticated!')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+db.sync()
+    .then(() => {
+        console.log('Database Synced')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Ok!'})
 }) 
+
+app.use('/api/v1', usersRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
