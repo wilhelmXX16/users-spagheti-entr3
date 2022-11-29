ed60a9393ff22220692f4754cc782a69cbaac829
wilhelmXX16 <wjc1619t@gmail.com>
2022-11-29 17:32:15 -0500

    finish services

--- src/users/users.services.js
+++ src/users/users.services.js
@@ -1,22 +1,77 @@
+const usersControllers = require('./users.controllers')
 
-const getAllUsers = () => {
 
+const getAllUsers = (req, res) => {
+    usersControllers.findAllUsers()
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch((err) => {
+            res.status(400).json({ message: err.message})
+        })
 }
 
-const getUserById = () => {
-    
+const getUserById = (req, res) => {
+    const id = req.params.id
+    usersControllers.findUserById(id)
+        .then(data => {
+            if(data){
+                res.status(200).json(data)
+            } else {
+                res.status(404).json({message: 'Invalid ID'})
+            }
+        })
+        .catch( err => {
+            res.status(400).json({ message: err.message })
+        })
 }
 
-const postUser = () => {
-    
+const postUser = (req, res) => {
+    const { first_name, last_name, email, password, birthday } = req.body
+    usersControllers.createUser({ first_name, last_name, email, password, birthday })
+        .then((data) => {
+            res.status(201).json(data)
+        })
+        .catch( err => {
+            res.status(400).json({ message: err.message })
+        })
 }
 
-const patchUser = () => {
-    
+const patchUser = (req, res) => {
+    const id = req.params.id
+    const { first_name, last_name, email, password, birthday } = req.body
+    usersControllers.updateUser(id , { first_name, last_name, email, password, birthday })
+        .then((data) => {
+            if(data){
+                res.status(200).json({message: 'Quote updated succesfully with id: ' + id})
+            } else {
+                res.status(404).json({message: 'Invalid ID'})
+            }
+        })
+        .catch(err => {
+            res.status(400).json({ message: err.message })
+        })
 }
 
-const deleteUser = () => {
-    
+const deleteUser = (req, res) => {
+    const id = req.params.id
+    usersControllers.deleteUser(id)
+        .then(data => {
+            if(data){
+                res.status(204).json({message: 'Deleted Succesfully'})
+            } else {
+                res.status(404).json({message: 'Invalid ID'})
+            }
+        })
+        .catch(err => {
+            res.status(400).json({ message: err.message }); 
+        })
 }
 
-
+module.exports = {
+    getAllUsers,
+    getUserById,
+    postUser,
+    patchUser,
+    deleteUser
+}
